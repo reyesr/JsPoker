5234041feae1a52a7a3a67ef678378d70295e844
Rodrigo <reyesrr@gmail.com>
2015-01-21 09:51:54 +0100

    Adds an http front-end to access the bots as remote, and displays their respective url

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require("body-parser");
+var fs = require("fs"),
+    path = require("path");
+
+var port = 3003;
+
+server.listen(port);
+app.use(bodyParser.json());
+
+var robots = fs.readdirSync("./players").map(function(file){
+    try {
+        var basename = path.basename(file, ".js");
+        return basename.length>0 ? {name: basename, bot: require("./players/" + file)()} : null;
+    } catch (err) {
+        console.error("Error loading bot " + file + " (ignored)");
+        return null;
+    }
+}).filter(function(bot){return bot != null});
+
+
+robots.forEach(function(botDescriptor) {
+    var name = botDescriptor.name;
+    var bot = botDescriptor.bot;
+    console.log("Initializing bot", name);
+
+    app.get('/bot/' + name, function(req, res) {
+        console.log("Serving info request for " + name);
+        res.status(200).json({info: bot.info});
+    });
+
+    app.post('/bot/' + name, function(req, res) {
+        var game = req.body;
+        var bet = bot.update(game);
+        res.json(200, {bet: bet});
+    });
+});
+
+app.use("/", function (req, res) {
+    var index_html = "<html><body><pre>";
+    index_html += "Hi. I serve the following bots:\n\n";
+    robots.forEach(function(robot){
+        index_html += req.protocol + "://" + req.hostname + (port!=80?":"+port:"") + "/bot/" + robot.name + "\n";
+    });
+    index_html += "</pre></body></html>";
+    res.status(200).send(index_html);
+});
+
+console.log("Server ready, listening on port", port);
